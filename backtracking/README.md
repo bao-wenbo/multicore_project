9c54c410960dcb0341801889c3a7272a2ef1c487
yosemitii <lubin.suen@gmail.com>
2023-04-19 14:47:49 -0400

    modified load balance methods, added referenced code

--- backtracking/README.md
+++ backtracking/README.md
@@ -1,3 +1,5 @@
+The reference code is from: https://rosettacode.org/wiki/N-queens_problem#C++.
+
 # How to compile
 
 The two cpp file are for two different problem. nqueens.cpp is to find all the solution of an n-queens problem. nqueens_first.cpp is to find the first X (e.g. X = 1, 3, 5) solution of an n-queens problem.
@@ -8,6 +10,8 @@ The two cpp file are for two different problem. nqueens.cpp is to find all the s
 
 2. Use "make" to compile 
 
-3. To use nqueens, type "./nqueens num_threads N"
+3. To find all the solutions of a N-queens problem, run `./nqueens num_threads N`, where num_threads is the number of threads, N is the number of queens.
 
-4. To use nqueens_first, type "./nqueens_first num_threads N X", where num_threads is the number of threads, N is the number of queens, X is the first X solution.
+4. To find X solutions of a N-queens problem, run `./nqueens num_threads N X`, where X is the first X solution.
+
+5. To test the reference code for a N-queens problem, run `nqueens_ref N`.
--- backtracking/nqueens.cpp
+++ backtracking/nqueens.cpp
@@ -4,22 +4,27 @@
 #include <cmath>
 #include <iostream>
 #include <omp.h>
+#include <climits>
 using namespace std;
 
 
 class Solution {
 public:
     vector<vector<string>> res;
+    int found = 0;
+    int queens_to_find = INT_MAX;
 
     vector<vector<string>> solveNQueens(int num_threads, int n) {
         vector<string> board(n, string(n, '.'));
         // backtracking(0, 0, n, board);
+        // printf("Solutions to find: %d\n", queens_to_find);
         #pragma omp parallel num_threads(num_threads)
-        divideBacktracking(num_threads, n);
+        loadBalance_v2(num_threads, n);
+        // loadBalance(num_threads, n);
         return res;
     }
 
-    void divideBacktracking(int thread_num, int n) {
+    void loadBalance(int thread_num, int n) {
         int my_rank = omp_get_thread_num();
         int round = 1;
         vector<string> local_board(n, string(n, '.'));
@@ -33,10 +38,75 @@ public:
         }
     }
 
-    void backtracking(int row, int start_col, int n, vector<string> &board) {
-        if (row == n) {
-            #pragma omp critical
+    void loadBalance_v2(int thread_num, int n) {
+        int my_rank = omp_get_thread_num();
+        // int round = (n - 1) / thread_num;
+        vector<string> local_board(n, string(n, '.'));
+
+        int start_setting_count = 0;
+        //col0 is the col of row 0
+        for (int col0 = 0; col0 < n; col0++) {
+            //col1 is the col of row 1
+            for (int col1 = 0; col1 < n; col1++) {
+                if (col1 <= col0+1 && col1 >= col0-1) 
+                {
+                    //Invalid set
+                    continue;
+                } 
+                else 
+                {
+                    //This task should be done by this thread
+                    //Set the first two rows
+                    //There are n*(n-3)+2 unique start settings
+                    if (start_setting_count % thread_num == my_rank)
+                    {
+                        local_board[0][col0] = 'Q';
+                        local_board[1][col1] = 'Q';
+                        //Start from row 2.
+                        backtracking_v2(2, n, local_board);
+                        local_board[0][col0] = '.';
+                        local_board[1][col1] = '.';
+                    }
+                    //Not critical because it is only used for assign threads.
+                    start_setting_count++;
+                }
+            }
+        }
+
+        // printf("%d start settings are set.\n", start_setting_count);
+
+    }
+
+    void backtracking_v2(int row, int n, vector<string> &board) {
+        if (found >= queens_to_find)
+            return;
+        if (row == n)
+        {
+        #pragma omp critical
             res.push_back(board);
+            found ++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(row, col, n, board)) {
+                // path.push_back(vector<int> vect1{row, col});
+                board[row][col] = 'Q';
+                backtracking_v2(row + 1, n, board);
+                board[row][col] = '.';
+            }
+        }
+        
+    }
+
+    void backtracking(int row, int start_col, int n, vector<string> &board) {
+        if (found >= queens_to_find)
+            return;
+        if (row == n)
+        {
+        #pragma omp critical
+            res.push_back(board);
+            found ++;
             return;
         }
 
@@ -78,23 +148,20 @@ public:
 };
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        printf("Wrong Input: Use ./nqueens num_threads num_queens");
+    if (argc != 4 && argc != 3) {
+        printf("Wrong Input: Use ./nqueens num_threads num_queens [num_unique]");
         return -1;
     }
     int num_queens;
     int num_threads;
+    int sol_to_find;
     sscanf(argv[1], "%d", &num_threads);
     sscanf(argv[2], "%d", &num_queens);
-
     Solution s;
+    s.queens_to_find = INT_MAX;
     s.solveNQueens(num_threads, num_queens);
-    // for (auto i = s.res.begin(); i != s.res.end(); ++i) {
-    //     for (auto j = i->begin(); j != i->end(); j++) {
-    //         cout << *j << endl;
-    //     }
-    //     cout << endl;
-    // }
-
-    cout << "Solution size: " << s.res.size() << endl;
+    if (argc == 4) {
+        sscanf(argv[3], "%d", &sol_to_find);
+    }
+    cout << "Solution found: " << s.res.size() << endl;
 }
